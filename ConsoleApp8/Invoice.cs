8a8ed903b16e762e93c6e5907d0c62345bdf08d8
shahnigarek <shahnigarek@code.edu.az>
2022-07-11 18:49:06 +0400

    second task

---
+++ ConsoleApp8/Invoice.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+     class Invoice
+    {
+        private string account;
+        private string customer;
+        private string provider;
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+        public Invoice(string account,string customer,string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+
+        public Invoice()
+        {
+
+        }
+        public double CostCalculation(bool needEdv)
+        {
+            double price = 0;
+            double edv = 0.18;
+            if (Article == "laptop")
+            {
+                price = 1400;
+            }
+           else if(Article== "SD")
+            {
+                price = 30;
+            }
+               else
+            {
+                price = 12;
+            }
+
+
+
+            if (needEdv == true)
+            {
+                price = price * Quantity + edv;
+            }
+
+
+            else
+            {
+                price = price * Quantity;
+            }
+        
+            return price;
+        }
+
+            
+
+           
+             
+         }
+
+
+        }
+    
+   
+
+
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Invoice inv = new Invoice("45378","Boby","Barlet");
+            inv.CostCalculation(true);
+            inv.Article="laptop";
+            inv.Quantity = 6;
+            Console.WriteLine(inv.CostCalculation(true));
+            Console.WriteLine(inv.Article);
+        }
+    }
+}
